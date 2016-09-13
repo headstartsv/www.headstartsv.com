78465823b5173a20111ff2fdfa94847a742797fa
Choi KyuWoo <apple@paio.co.kr>
2016-09-13 21:50:47 +0900

    more info on bootcamp item cards

--- app/controllers/bootcamp.js
+++ app/controllers/bootcamp.js
@@ -13,56 +13,68 @@ router.get('/', function (req, res, next) {
       id: '1',
       loc: 'US',
       title: 'Data Science Bootcamp',
+      subject: 'Big Data Regression',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Advanced',
+      target: '학교 / 기업 / 관공서'
     }, {
       id: '2',
       loc: 'Korea',
       title: 'Data Science Bootcamp',
+      subject: 'Basic R & Python',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Beginner',
+      target: '학교 / 기업 / 관공서'
     }, {
       id: '3',
       loc: 'US',
       title: 'Data Science Bootcamp',
+      subject: 'Data Analysis & Visualization',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Advanced',
+      target: '학교 / 기업 / 관공서'
     }], [{
       id: '4',
       loc: 'Korea',
       title: 'Data Science Bootcamp',
+      subject: 'Big Data Regression ',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Beginner',
+      target: '학교 / 기업 / 관공서'
     }, {
       id: '5',
       loc: 'US',
       title: 'Data Science Bootcamp',
+      subject: 'Big Data Regression ',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Advanced',
+      target: '학교 / 기업 / 관공서'
     }, {
       id: '6',
       loc: 'Korea',
       title: 'Data Science Bootcamp',
+      subject: 'Big Data Regression ',
       campStart: '2016. 12. 28',
       campEnd: '2017. 1. 24',
       applyStart: '2016. 11. 1',
       applyEnd: '2016. 12. 10',
-      target: 'Advanced'
+      class: 'Advanced',
+      target: '학교 / 기업 / 관공서'
     }]]
   });
 });
