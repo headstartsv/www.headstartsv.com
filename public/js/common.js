c7f64d44e3c3972f3b2090fcd67c010a4c7bd16c
Choi KyuWoo <apple@paio.co.kr>
2016-09-18 10:28:00 +0900

    show under construction alert for 'events', 'alumni' menu.

--- public/js/common.js
+++ public/js/common.js
@@ -2,4 +2,8 @@ $(function () {
   $('.drawer-nav > li > a').click(function () {
     $('#drawer').drawer('hide');
   });
+  $('.under-construction').click(function () {
+    alert('서비스 준비 중 입니다.');
+    return false;
+  });
 });
